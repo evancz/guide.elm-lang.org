14ef0e9cb48ef63afe29c920f45a0c61baae70ae
Evan Czaplicki <evancz@users.noreply.github.com>
2019-09-20 16:07:43 -0400

    have the build script build the REPL

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+set -e
+
+
+## BUILD REPL
+
+(cd repl ; npm link ; bash build.sh)
+
+
+## BUILD BOOK
+
 npm install gitbook-cli@2.3.2
 ./node_modules/.bin/gitbook install
+npm link gitbook-plugin-elm-repl
+sed -i 's/"youtube"/"youtube","elm-repl"/' package.json
 ./node_modules/.bin/gitbook build
+
+
